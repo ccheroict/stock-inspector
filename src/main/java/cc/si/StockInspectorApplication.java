525e20d013262b67dbe4cb9cdae40e90c6c7392b
Quang Chung <leadotc@gmail.com>
2016-06-07 15:07:15 +0200

    add log4j2

--- src/main/java/cc/si/StockInspectorApplication.java
+++ src/main/java/cc/si/StockInspectorApplication.java
@@ -1,10 +1,21 @@
 package cc.si;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StockInspectorApplication {
+public class StockInspectorApplication extends SpringBootServletInitializer {
+
+    private static final Logger logger = LogManager.getLogger(StockInspectorApplication.class);
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(StockInspectorApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(StockInspectorApplication.class, args);
