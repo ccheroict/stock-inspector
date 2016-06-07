cdd3b18f54f086fd465a94809825f270d7015c21
Quang Chung <leadotc@gmail.com>
2016-06-07 13:10:30 +0200

    initial commit

---
+++ src/main/java/cc/si/StockInspectorApplication.java
@@ -0,0 +1,12 @@
+package cc.si;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockInspectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockInspectorApplication.class, args);
+    }
+}
